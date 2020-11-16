eca9da68c16da3ec679845dca861c57f7ff768fe
Armin Kazemi <arminkz@live.com>
2020-11-16 19:54:22 +0330

    AP on/off scripts

---
+++ activate-ap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+wifidev="wlan0"
+
+createHotspot()
+{
+    ip link set dev "$wifidev" down
+    ip a add 192.168.220.1/24 brd + dev "$wifidev"
+    ip link set dev "$wifidev" up
+    dhcpcd -k "$wifidev" >/dev/null 2>&1
+    systemctl start dnsmasq
+    systemctl start hostapd
+}
+
+if systemctl status hostapd | grep "(running)" >/dev/null 2>&1
+then
+    echo "Hostspot already active"
+elif { wpa_cli status | grep "$wifidev"; } >/dev/null 2>&1
+then
+	echo "Cleaning wifi files and Activating Hotspot..."
+	wpa_cli terminate >/dev/null 2>&1
+	ip addr flush "$wifidev"
+	ip link set dev "$wifidev" down
+	rm -r /var/run/wpa_supplicant >/dev/null 2>&1
+	createHotspot
+else #"No SSID, activating Hotspot"
+	echo "Activating Hotspot..."
+    createHotspot
+fi
---
+++ deactivate-ap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+wifidev="wlan0"
+
+killHotspot()
+{
+    echo "Shutting Down Hotspot"
+    ip link set dev "$wifidev" down
+    systemctl stop hostapd
+    systemctl stop dnsmasq
+    ip addr flush dev "$wifidev"
+    ip link set dev "$wifidev" up
+    dhcpcd  -n "$wifidev" >/dev/null 2>&1
+}
+
+if systemctl status hostapd | grep "(running)" >/dev/null 2>&1
+then #hotspot running and ssid in range
+	killHotspot
+	echo "Hotspot Deactivated, Bringing Wifi Up"
+	wpa_supplicant -B -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf >/dev/null 2>&1
+elif { wpa_cli -i "$wifidev" status | grep 'ip_address'; } >/dev/null 2>&1
+then #Already connected
+    echo "Wifi already connected to a network"
+else #ssid exists and no hotspot running connect to wifi network
+	echo "Connecting to the WiFi Network"
+	wpa_supplicant -B -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf >/dev/null 2>&1
+fi
